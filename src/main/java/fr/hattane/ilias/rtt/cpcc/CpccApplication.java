cae7576ca68f506a6541ac7f6db701ae8e6be5cf
drakonic59 <iliasdavid@live.fr>
2025-08-05 19:50:33 +0200

    Initialisation

---
+++ src/main/java/fr/hattane/ilias/rtt/cpcc/CpccApplication.java
@@ -0,0 +1,13 @@
+package fr.hattane.ilias.rtt.cpcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpccApplication.class, args);
+	}
+
+}
