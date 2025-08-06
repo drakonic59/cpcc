9d36d0cbbca8952db51bd626985c8f4bab1b6f01
drakonic59 <iliasdavid@live.fr>
2025-08-06 19:52:16 +0200

    configuration mvc pour les langues et autres assets

---
+++ src/main/java/fr/hattane/ilias/rtt/cpcc/config/MvcConfig.java
@@ -0,0 +1,43 @@
+package fr.hattane.ilias.rtt.cpcc.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.FRANCE);
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localChangeInterceptor());
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
+}
